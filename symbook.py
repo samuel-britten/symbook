20738bf57b60e28b0ce81b1a885064c12852907d
Samuel_Britten <britten.samuelj@gmail.com>
2025-01-02 16:38:50 -0700

    plotly

--- symbook.py
+++ symbook.py
@@ -2,7 +2,6 @@ import streamlit as st
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor
 from sympy import *
-from bokeh.plotting import *
 from spb import *
 import streamlit as st
 from streamlit.components.v1 import html
